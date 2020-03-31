30a022006db40c5b3ed165c297569e5900f86374
肖杰 <xiaojie@sansi.com>
2020-03-31 18:18:50 +0800

    feat(tempelate):增加template模式demo

---
+++ Src/Template/cresumea.cpp
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Whuer_XiaoJie <1939346428@qq.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+#include "cresumea.h"
+#include <iostream>
+
+using namespace std;
+
+CResumeA::CResumeA()
+{
+
+}
+CResumeA:: ~CResumeA()
+{
+
+}
+
+void CResumeA::setPersonalInfo()
+{
+	cout << "A has set personal information" << endl;
+}
+
+void CResumeA::setEducation()
+{
+	cout << "A has set education" << endl;
+}
+
+void CResumeA::setWorkExp()
+{
+	cout << "A has set work experience" << endl;
+}
